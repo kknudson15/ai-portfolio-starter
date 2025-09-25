1f76d8355321272e4b7b5b455ad9a82fbd07f7f2
Kyle Knudson <kyle.knudson2015@gmail.com>
2025-09-25 14:25:13 -0500

    Incorrect pathing fix

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -5,7 +5,7 @@ import Nav from "@/components/Nav";
 import Footer from "@/components/footer";
 import Link from "next/link";
 import slugify from "../lib/slugify";
-import knowledgeBase from "../../data/knowledgeBase";
+import knowledgeBase from "../data/knowledgeBase";
 
 const projects = knowledgeBase.projects;
 
